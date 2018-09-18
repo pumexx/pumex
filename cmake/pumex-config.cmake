bb4540744d658c2a8cdfa93564fda988737af712
Pawel <pawel@black.pearl>
2018-09-18 19:47:13 +0200

    Linux fix

--- cmake/pumex-config.cmake
+++ cmake/pumex-config.cmake
@@ -9,8 +9,18 @@ get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
 if(_IMPORT_PREFIX STREQUAL "/")
   set(_IMPORT_PREFIX "")
 endif()
+
+set( _FTINCLUDEDIRS )
+if(UNIX AND NOT APPLE)
+  find_package(Freetype)
+  set( _FTINCLUDEDIRS "${FREETYPE_INCLUDE_DIRS}" )
+else()
+  set( _FTINCLUDEDIRS "${_IMPORT_PREFIX}/include;${_IMPORT_PREFIX}/include/freetype2;" )
+endif()
+
 set_target_properties(pumex::pumexlib PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include;${_IMPORT_PREFIX}/include/freetype2"
+  INTERFACE_INCLUDE_DIRECTORIES "${_FTINCLUDEDIRS}"
 )  
 # Cleanup temporary variables.
-set(_IMPORT_PREFIX)  
+set( _FTINCLUDEDIRS )
+set( _IMPORT_PREFIX )
